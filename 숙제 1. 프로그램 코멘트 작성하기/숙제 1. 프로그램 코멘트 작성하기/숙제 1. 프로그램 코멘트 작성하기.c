99ca148a37606195445fbafd6ae1904c78b7e867
sujing-jing <121999086+sujing-jing@users.noreply.github.com>
2026-09-13 15:42:56 +0900

    Assignment1 file upload
    숙제 1. 프로그램 코멘트 작성하기 프로젝트 파일 업로드

---
+++ 숙제 1. 프로그램 코멘트 작성하기/숙제 1. 프로그램 코멘트 작성하기/숙제 1. 프로그램 코멘트 작성하기.c
@@ -0,0 +1,19 @@
+/*
+File: 숙제1. 프로그램 코멘트 작성하기
+Name: Seo-jin Jeon
+Student identification number: 2021440120
+Class: D
+Date: March 14th, 2021
+Revision:
+	0.0: by Seo-jin Jeon
+Others: I deleted the Mcfree program because code was not executed because of it...
+*/
+
+#include <stdio.h>
+
+main()
+{
+	printf("이름: 전서진\n");
+	printf("학번: 2021440120\n");
+	printf("프로그래밍 날짜: 2021.03.14\n");
+}
