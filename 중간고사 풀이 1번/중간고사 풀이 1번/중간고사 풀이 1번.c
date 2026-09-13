0c2975a4d5d871765a6fc49f700b5d1f63fa5bd3
sujing-jing <121999086+sujing-jing@users.noreply.github.com>
2026-09-13 16:08:46 +0900

    Midterm Exam file upload
    중간고사 풀이 1번 프로젝트 파일 업로드

---
+++ 중간고사 풀이 1번/중간고사 풀이 1번/중간고사 풀이 1번.c
@@ -0,0 +1,22 @@
+/*
+File: 중간고사 풀이 1번
+Name: Seo-jin Jeon
+Student identification number: 2021440120
+Class: 04분반
+Date: 2021.05.04
+Revision:
+	0.0: by Seo-jin Jeon
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	double pi = 3.141592, date = 20210421.;
+	printf("01234567890123456789\n");
+	printf("%012.2f\n", pi);
+	printf("%-012.2f\n", pi);
+	printf("%+12.4e\n", date);
+	printf("%+12d\n", (int)date);
+	return 0;
+}
